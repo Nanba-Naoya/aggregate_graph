a5a914c8860cd0869d3271541669a44d1850f679
Nanbeee <naoyamessi10@gmail.com>
2018-12-10 14:32:39 +0900

    時間を計算するAPI

--- app/controllers/api/v1/show_graph_controller.rb
+++ app/controllers/api/v1/show_graph_controller.rb
@@ -3,9 +3,7 @@ module Api::V1
 
     def index
       work_times = select_work_time
-      if work_times.empty?
-        work_times['error'] = true;
-      end
+      work_times['error'] = true if work_times.empty?
       render json: work_times
     end
 
@@ -14,39 +12,34 @@ module Api::V1
     def select_work_time
       user_id = 1111
 
-      params[:type_flag] == 'false' ? work_times = calctime(user_id) : work_times = calctime_category(user_id)
+      work_times = params[:type_flag] == 'false' ? calctime(user_id) : calctime_category(user_id)
       work_times
     end
 
     def calctime(user_id)
-      category_ids = []
+      category_names = []
       work_times = {}
 
-      params[:day].nil? ? work_dates = WorkTime.of_work_time(user_id, change_time(params[:month])) : work_dates = WorkTime.of_work_time(user_id, "#{change_time(params[:month])}-#{change_time(params[:day])}")
-
-      work_dates.each do |work_date|
-        category_ids.include?(work_date['category_id']) ?  category_ids << work_date['category_id'] : category_ids << work_date['category_id']
-        category_name = Category.find(work_date['category_id'])
-        category_ids.include?(work_date['category_id']) ? work_times["#{category_name['title']}"] = work_times["#{category_name['title']}"].to_f + work_date['time'].to_f : work_times["#{category_name['title']}"] = work_date['time'].to_f
-      end
+      work_datas = params[:day].nil? ? WorkTime.of_calc(user_id, change_time(params[:month])) : WorkTime.of_calc(user_id, "#{change_time(params[:month])}-#{change_time(params[:day])}")
+      category_names = Category.of_name(work_datas.keys)
+      #hashに変換
+      work_times = [category_names, work_datas.values].transpose
+      work_times = Hash[*work_times.flatten]
       work_times
     end
 
     def calctime_category(user_id)
       work_times = {}
 
-      params[:day].nil? ? work_dates = WorkTime.of_category_time(user_id, params[:category_id], change_time(params[:month])) : work_dates = WorkTime.of_category_time(user_id, params[:category_id], "#{change_time(params[:month])}-#{change_time(params[:day])}")
-      
-      work_dates.each do |work_date|
-        category_name = Category.find(work_date['category_id'])
-        work_times["#{category_name['title']}"] = work_times["#{category_name['title']}"].to_f + work_date['time'].to_f
-      end
+      work_datas = params[:day].nil? ? WorkTime.calc_category(user_id, params[:category_id], change_time(params[:month])) : WorkTime.calc_category(user_id, params[:category_id], "#{change_time(params[:month])}-#{change_time(params[:day])}")
+      category_name = Category.find(params[:category_id])
+      work_times["#{category_name['title']}"] = work_datas
       work_times
     end
 
     def change_time(times)
-      times = "0#{times}" if times.to_i <= 9
-      times
+      return times unless times.to_i <= 9
+      "0#{times}"
     end
 
   end
--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -2,7 +2,7 @@ class WorkTime < ApplicationRecord
   belongs_to :category, optional: true
   validates :time, presence: true
 
-  scope :of_work_time, ->(user_id, date) { where("user_id = #{user_id} and created_at LIKE ?", "2018-#{date}%")}
-  scope :of_category_time, ->(user_id, category_id, date) { where("user_id = #{user_id} and category_id = #{category_id} and created_at LIKE ?", "2018-#{date}%")}
+  scope :calc_category, ->(user_id, category_id, date) { where("user_id = #{user_id} and category_id = #{category_id} and created_at LIKE ?", "2018-#{date}%").sum(:time)}
+  scope :of_calc, ->(user_id, date) { where("user_id = #{user_id} and created_at LIKE ?", "2018-#{date}%").group(:category_id).sum(:time)}
   
 end
