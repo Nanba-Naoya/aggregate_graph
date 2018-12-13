5ff2c5b7da6fa34a00dc47f82ebf317c795ecea3
Naoya Nanba <cl-nanba@classi-no-MacBook-Pro.local>
2018-12-13 11:22:20 +0900

    テストファイル修正

--- spec/controllers/api/v1/categories_controller_spec.rb
+++ spec/controllers/api/v1/categories_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CategoriesController, type: :controller do
   
-  let!(:category1) do 
+  let!(:categories) do 
     [
       FactoryGirl.create(:category, title: '会議'),
       FactoryGirl.create(:category, title: '資料作成')
@@ -14,17 +14,18 @@ RSpec.describe Api::V1::CategoriesController, type: :controller do
       get :index
     end
     it '必要な値がセットされている' do
-      expect(controller.instance_variable_get('@categories')).to eq category1[0..1]
+      expect(JSON.parse(response.body).map{|item| item['title']}).to eq(['会議', '資料作成'])
     end
   end
 
   describe "POST #create" do
     before do
-      get :create, params: {category: {title: category1[0].title}}
+      post :create, params: {category: {title: 'テスト'}}
     end
     it 'responseが正しいか' do
       expect(JSON.parse(response.body)['message']).to eq('ok')
       expect(JSON.parse(response.body)['status']).to eq(200)
+      expect(Category.last.title).to eq 'テスト'
     end
   end
 
