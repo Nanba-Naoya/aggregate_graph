e3fed83518fda19db7568ee0aaf18cacbd5bb9a0
Nanbeee <naoyamessi10@gmail.com>
2019-01-24 18:23:58 +0900

    現在時刻を指定して入れていた部分を修正

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -12,8 +12,7 @@ module Api::V1
         render json: { message: I18n.t('category_exist'), status: 400 }
         return
       end
-      category = Category.new(title: params[:title], created_at: Time.current,
-                              updated_at: Time.current, user_id: params[:user_id])
+      category = Category.new(title: params[:title], user_id: params[:user_id])
       category.save!
       render json: { message: I18n.t('create_category_message'), status: 200 }
     rescue ActiveRecord::RecordInvalid => e
