31bcd37827e3fa443bd310d27214c8a4374fd27e
Nanbeee <naoyamessi10@gmail.com>
2019-02-26 18:16:13 +0900

    カレンダーを表示して期間を選択できるようにライブラリを追加

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module'; 
+import { AppRoutingModule } from './app-routing.module';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpsInterceptor } from './shared/services/http.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { Ng2FlatpickrModule } from 'ng2-flatpickr';
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './components/top-menu.component';
 import { ShowGraphComponent } from './components/show-graph/show-graph.component';
 import { InputDateComponent } from './components/input-date/input-date.component';
-import { CreateCategoryComponent } from './components/create-category/create-category.component';
 
 import { InputDateService } from './components/services/input-date.service';
-import { CreateCategoryService } from './components/services/create-category.service';
 import { ShowGraphService } from './components/services/show-graph.service';
 import { ShowCategoryGraphComponent } from '../app/components/show-category-graph/show-category-graph.component';
 import { ShowNormalGraphComponent } from '../app/components/show-normal-graph/show-normal-graph.component';
@@ -27,7 +26,6 @@ import { SelectDateComponent } from './shared/components/select-date/select-date
     ShowGraphComponent,
     TopMenuComponent,
     InputDateComponent,
-    CreateCategoryComponent,
     ShowCategoryGraphComponent,
     ShowNormalGraphComponent,
     SelectDateComponent
@@ -43,11 +41,11 @@ import { SelectDateComponent } from './shared/components/select-date/select-date
       timeOut: 2000,
       positionClass: 'toast-top-center',
       preventDuplicates: false
-    })
+    }),
+    Ng2FlatpickrModule,
   ],
   providers: [
     InputDateService,
-    CreateCategoryService,
     ShowGraphService,
     CookieService,
     { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true },
